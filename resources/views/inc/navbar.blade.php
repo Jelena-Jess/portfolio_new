bdb383c107d1d48999b1223ea4b9971f60b15e57
Jelena Petkov <petkovjelena@gmail.com>
2020-05-12 16:20:06 +0200

    navbar-fixed: color, dropdown changed

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -10,10 +10,10 @@
           <a class="nav-link hvr-underline-from-center" href="/">HOME
           </a>
         </li>
-        <li class="dropdown mydropdowncss px-xl-2 px-lg-0 nav-item bd-highlight nav">
-          <a class="nav-link" href="">SERVICES
+        <li class="dropdown px-xl-2 px-lg-0 nav-item nav">
+          <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SERVICES
           </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <a class="dropdown-item nav-link-small hvr-underline-from-center" href="/website">WEBSITE DESIGN</a>
             <a class="dropdown-item nav-link-small hvr-underline-from-center" href="/logo">LOGO DESIGN</a>
             <a class="dropdown-item nav-link-small hvr-underline-from-center" href="/content">CONTENT CREATION</a>
